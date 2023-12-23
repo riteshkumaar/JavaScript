9ed47a10e059a8650bf7777f5836ea3ed3f2895b
riteshkumaar <146605213+riteshkumaar@users.noreply.github.com>
2023-12-23 03:09:07 +0000

    data types

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"
+// use newer version of JavaScript
+// alert(3+3) - We are using node js and not browser so this doesn't work
+// this works while inspecting the element
+console.log(3+3)
+// code should be readable
+let name="Ritesh"
+let age=18
+let isLoggedIn= false
+// numbers lie in range 2^51
+// bigint ->to got beyond the above range
+// string -> ""
+//  boolean ->true/false
+//  null is a standalone value -> generally means not available
+// undefined - value hasn't been assigned
+// symbol - used for uniqueness
+// objects 
+// type of null -> objects & type of undefined ->undefined
+console.log(typeof age)
